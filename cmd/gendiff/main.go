5f54af191fe40985f4197f071ccb4dfd2a142224
i1yas <ilyasers@gmail.com>
2026-07-31 08:44:37 +0300

    init

---
+++ cmd/gendiff/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:  "gendiff",
+		Usage: "Compares two configuration files and shows a difference.",
+		Action: func(_ context.Context, cmd *cli.Command) error {
+			fmt.Printf("TODO")
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+}
