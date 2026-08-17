bdc464af20cb3d29e6f1ca7ec8724abdc1f37da2
i1yas <ilyasers@gmail.com>
2026-08-17 19:00:17 +0300

    refactor: move command action to separate function

--- internal/app/command.go
+++ internal/app/command.go
@@ -29,23 +29,25 @@ func BuildCommand() *cli.Command {
 				DefaultText: defaultFormat,
 			},
 		},
-		Action: func(_ context.Context, cmd *cli.Command) error {
-			if cmd.Args().Len() != 2 {
-				return fmt.Errorf("%w: got %d", ErrExpectingTwoPaths, cmd.Args().Len())
-			}
-
-			format := cmd.String("format")
-
-			diff, err := code.GenDiff(cmd.Args().Get(0), cmd.Args().Get(1), format)
-			if err != nil {
-				return err
-			}
-
-			fmt.Fprintln(cmd.Root().Writer, diff)
-
-			return nil
-		},
+		Action: genDiffAction,
 	}
 
 	return cmd
+}
+
+func genDiffAction(_ context.Context, cmd *cli.Command) error {
+	if cmd.Args().Len() != 2 {
+		return fmt.Errorf("%w: got %d", ErrExpectingTwoPaths, cmd.Args().Len())
+	}
+
+	format := cmd.String("format")
+
+	diff, err := code.GenDiff(cmd.Args().Get(0), cmd.Args().Get(1), format)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintln(cmd.Root().Writer, diff)
+
+	return nil
 }
