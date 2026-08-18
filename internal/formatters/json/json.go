b31c24c48f92391c81d47da91ec663ba8285ec4b
i1yas <ilyasers@gmail.com>
2026-08-18 16:08:51 +0300

    feat: changed json format

--- internal/formatters/json/json.go
+++ internal/formatters/json/json.go
@@ -6,11 +6,23 @@ import (
 	"code/internal/diff"
 )
 
-type diffItemJSON map[string]any
+type diffItemJSON struct {
+	Key      string         `json:"key"`
+	Type     string         `json:"type"`
+	Value1   any            `json:"value1,omitempty"`
+	Value2   any            `json:"value2,omitempty"`
+	Children []diffItemJSON `json:"children,omitempty"`
+}
 
 // Format formats diff to json representation.
 func Format(df []diff.Item) string {
-	jsonDiff := convertDiffToJSON(df)
+	rootChildren := convertDiffToJSON(df)
+
+	jsonDiff := diffItemJSON{
+		Key:      "",
+		Type:     "root",
+		Children: rootChildren,
+	}
 
 	data, _ := json.Marshal(jsonDiff)
 
@@ -29,53 +41,57 @@ func convertDiffToJSON(df []diff.Item) []diffItemJSON {
 
 func convertDiffItemToJSON(item diff.Item) diffItemJSON {
 	diffItem := diffItemJSON{
-		"key":    item.Key,
-		"change": getHumanReadbleChange(item.Change),
+		Key:  item.Key,
+		Type: getHumanReadbleChange(item),
 	}
 
 	if item.Nested != nil {
 		jsonDiff := convertDiffToJSON(*item.Nested)
 
-		diffItem["nested"] = jsonDiff
+		diffItem.Children = jsonDiff
 
 		return diffItem
 	}
 
 	if item.Change == diff.ItemChangeAdd {
-		diffItem["value"] = item.NewValue
+		diffItem.Value2 = item.NewValue
 
 		return diffItem
 	}
 
 	if item.Change == diff.ItemChangeRemove {
-		diffItem["value"] = item.OldValue
+		diffItem.Value1 = item.OldValue
 
 		return diffItem
 	}
 
 	if item.Change == diff.ItemChangeReplace {
-		diffItem["old_value"] = item.OldValue
-		diffItem["new_value"] = item.NewValue
+		diffItem.Value1 = item.OldValue
+		diffItem.Value2 = item.NewValue
 
 		return diffItem
 	}
 
-	diffItem["value"] = item.OldValue
+	diffItem.Value1 = item.OldValue
 
 	return diffItem
 }
 
-func getHumanReadbleChange(change diff.ItemChange) string {
-	switch change {
+func getHumanReadbleChange(item diff.Item) string {
+	if item.Nested != nil {
+		return "nested"
+	}
+
+	switch item.Change {
 	case diff.ItemChangeAdd:
-		return "add"
+		return "added"
 	case diff.ItemChangeRemove:
-		return "remove"
+		return "deleted"
 	case diff.ItemChangeReplace:
-		return "replace"
+		return "changed"
 	case diff.ItemChangeNone:
-		return "none"
+		return "unchanged"
 	default:
-		return "none"
+		return "unchanged"
 	}
 }
